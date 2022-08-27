f3307c054a666babcf5c293bd1c5b2631e679284
Dusan-O <dusan.orescanin@gmail.com>
2022-08-27 16:15:50 +0200

    Initial Commit

---
+++ JurassicDino/JurassicDinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JurassicDinoApp.swift
+//  JurassicDino
+//
+//  Created by Dusan Orescanin on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct JurassicDinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
